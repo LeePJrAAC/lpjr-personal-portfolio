753f287f34ed77373356a8d52c151c93c0761ed7
L0ngshanks <lee.paulison.jr@gmail.com>
2024-02-04 18:07:44 -0500

    New-Edit form built

--- src/routes/app.jsx
+++ src/routes/app.jsx
@@ -1,12 +1,15 @@
-import { NavLink, Outlet, useLocation } from "react-router-dom";
+import { NavLink, Link, Outlet, useLocation } from "react-router-dom";
 
 export function App() {
   return (
     <div className='flex flex-row p-4'>
-      <div className='flex flex-col w-1/4'>
-        <NavLink to='/app/edit' className='nav-link text-stone-900'>
-          Edit
-        </NavLink>
+      <div className='flex flex-col w-1/3'>
+        <div className='flex flex-row space-x-4'>
+          <input type='text' />
+          <Link to='/app/edit' className='button' role='button'>
+            New
+          </Link>
+        </div>
         <NavLink to='/app/audio' className='nav-link text-stone-900'>
           Audio
         </NavLink>
@@ -14,7 +17,7 @@ export function App() {
           Video
         </NavLink>
       </div>
-      <div className='flex flex-col w-3/4'>
+      <div className='flex flex-col w-2/3 px-4'>
         {useLocation().pathname !== "/app" ? <Outlet /> : <h1>Welcome to the App</h1>}
       </div>
     </div>
--- src/routes/app/edit.jsx
+++ src/routes/app/edit.jsx
@@ -1,8 +1,28 @@
+import { Form } from "react-router-dom";
+
 export function Edit() {
   return (
-    <div className='p-4'>
-      <h1>Edit</h1>
-      <p>This is a simple example of how to use React Router to create a multi-page application.</p>
-    </div>
+    <>
+      <h1>Edit Media Listing</h1>
+      <Form className='flex flex-col space-y-4 mt-4'>
+        <input type='text' placeholder='Listing Name' />
+        <input type='text' placeholder='Media Title' />
+        <input type='text' placeholder='Media Description' />
+        <input type='text' placeholder='Media URL' />
+        <input type='text' placeholder='Image URL' />
+        <select>
+          <option value='audio'>Audio</option>
+          <option value='video'>Video</option>
+        </select>
+        <div className='flex flex-row space-x-4 ms-auto'>
+          <button className='button' role='button'>
+            Save
+          </button>
+          <button className='button bg-stone-500' role='button'>
+            Cancel
+          </button>
+        </div>
+      </Form>
+    </>
   );
 }
