3c184156329d1de973d6786917f1391de7c52629
L0ngshanks <lee.paulison.jr@gmail.com>
2024-02-04 19:14:52 -0500

    CRUD written

---
+++ src/media.js
@@ -0,0 +1,51 @@
+import localforage from "localforage";
+import { matchSorter } from "match-sorter";
+import sortBy from "sort-by";
+
+function set(media) {
+  return localforage.setItem("media", media);
+}
+
+export async function getAllMedia(query = "") {
+  let allMedia = await localforage.getItem("media");
+  if (!allMedia) allMedia = [];
+  if (query) {
+    allMedia = matchSorter(allMedia, query, { keys: ["name", "title", "description"] });
+  }
+  return allMedia.sort(sortBy("createdAt"));
+}
+
+export async function createMedia() {
+  let id = Math.random().toString(36).substring(2, 9);
+  let media = { id, createdAt: Date.now() };
+  let medias = await getAllMedia();
+  medias.unshift(media);
+  await set(medias);
+  return media;
+}
+
+export async function getAllMedia(id) {
+  let allMedia = await localforage.getItem("media");
+  let media = allMedia.find((media) => media.id === id);
+  return media ?? null;
+}
+
+export async function updateMedia(id, updates) {
+  let allMedia = await localforage.getItem("media");
+  let media = allMedia.find((media) => media.id === id);
+  if (!media) throw new Error("No media found for", id);
+  Object.assign(media, updates);
+  await set(allMedia);
+  return media;
+}
+
+export async function deleteMedia(id) {
+  let allMedia = await localforage.getItem("media");
+  let index = allMedia.findIndex((media) => media.id === id);
+  if (index > -1) {
+    allMedia.splice(index, 1);
+    await set(allMedia);
+    return true;
+  }
+  return false;
+}
